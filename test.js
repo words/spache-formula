200fea42ae08a044b5214d8767ef0dfe38f5278a
Titus Wormer <tituswormer@gmail.com>
2020-02-01 10:23:56 +0100

    Fix typo

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var test = require('tape')
 var nan = require('is-nan')
 var spache = require('.')
 
-test('daleChall', function(t) {
+test('spacheFormula', function(t) {
   t.ok(nan(spache()), 'NaN when an invalid value is given')
   t.equal(round(spache({word: 30, sentence: 2, unfamiliarWord: 6})), 4.114)
   t.equal(round(spache({word: 30, sentence: 2})), 2.474)
