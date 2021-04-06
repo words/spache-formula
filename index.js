3455732b15791f8b7acb7a3dda36945336105546
Titus Wormer <tituswormer@gmail.com>
2021-04-06 10:54:56 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+/**
+ * @typedef {Object} SpaceFormulaCounts
+ * @property {number} sentence
+ * @property {number} word
+ * @property {number} [unfamiliarWord]
+ */
+
 var sentenceWeight = 0.121
 var wordWeight = 0.082
 var percentage = 100
 var base = 0.659
 
-// Get the grade level of a given value according to the Spache Readability
-// Formula.
-// More information is available at WikiPedia:
-// <https://en.wikipedia.org/wiki/Spache_Readability_Formula>
+/**
+ * Get the grade level of a given value according to the Spache Readability Formula. More information is available at WikiPedia: <https://en.wikipedia.org/wiki/Spache_Readability_Formula>
+ *
+ * @param {SpaceFormulaCounts} counts
+ * @return {number}
+ */
 export function spacheFormula(counts) {
   if (!counts || !counts.sentence || !counts.word) {
     return Number.NaN
--- test.js
+++ test.js
@@ -2,6 +2,7 @@ import test from 'tape'
 import {spacheFormula} from './index.js'
 
 test('spacheFormula', function (t) {
+  // @ts-ignore runtime.
   t.ok(Number.isNaN(spacheFormula()), 'NaN when an invalid value is given')
   t.equal(
     round(spacheFormula({word: 30, sentence: 2, unfamiliarWord: 6})),
@@ -11,6 +12,10 @@ test('spacheFormula', function (t) {
   t.end()
 })
 
+/**
+ * @param {number} value
+ * @return {number}
+ */
 function round(value) {
   return Math.round(value * 1e6) / 1e6
 }
