899f67eef60d27e810bd286a338d1824dc6f1149
Titus Wormer <tituswormer@gmail.com>
2022-11-01 16:02:44 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-import test from 'tape'
+import assert from 'node:assert'
+import test from 'node:test'
 import {spacheFormula} from './index.js'
 
-test('spacheFormula', function (t) {
-  // @ts-ignore runtime.
-  t.ok(Number.isNaN(spacheFormula()), 'NaN when an invalid value is given')
-  t.equal(
+test('spacheFormula', function () {
+  // @ts-expect-error runtime.
+  assert.ok(Number.isNaN(spacheFormula()), 'NaN when an invalid value is given')
+  assert.equal(
     round(spacheFormula({word: 30, sentence: 2, unfamiliarWord: 6})),
     4.114
   )
-  t.equal(round(spacheFormula({word: 30, sentence: 2})), 2.474)
-  t.end()
+  assert.equal(round(spacheFormula({word: 30, sentence: 2})), 2.474)
 })
 
 /**
